ce9bbfe82a27d532140100970b34cd748cdc1eeb
ryancdavey1 <ryancdavey1@gmail.com>
2020-12-02 22:38:54 -0700

    commit tree cd70698a6707754ef0eeb62c2c686d2804331ab0

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+// Write your code here
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.soup = soup
+    this.salad = salad;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.soup = soup
+    this.salad = salad;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
